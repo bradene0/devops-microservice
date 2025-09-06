0b80fdb9d32b2689ca357b0e4f8b0003c36d769c
bradene0 <bradene0@outlook.com>
2025-09-06 11:04:11 -0500

    vpc, sec group, ecs, rds, s3 bucket

---
+++ infra/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "app_name" {
+  type    = string
+  default = "devops-microservice"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
